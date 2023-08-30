05025585c98437168f832b6418c4c2f7dfd7a7c2
shivani mishra <mishrasiddh22@gmail.com>
2023-08-31 01:45:08 +0530

    created API for retrieving and insertion data in db table users,HttpResponce for string hello world *

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, admin
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  
+    path( '',views.index,name='index',),
+    path('hello', views.hello, name='hello'),
+    path('users', views.users, name='users'),
+    path('new_users', views.new_users, name='new_users'),
+    path('info_users/<int:id>', views.info_users, name='info_users'),  
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,1 +1,34 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from .models import Xusers
+
+# front web page
+def index(request):
+    return render(request, 'index.html')
+
+
+# returns the string 'Hello Word' 
+def hello(request):
+    return HttpResponse('HELLO WORLD !!!')
+
+
+# A list of users from a MySQL database and display as a list in users.html page
+def users(request):
+    record = Xusers.objects.all()
+    context = {'record':record}
+    return render(request, 'users.html', context)
+
+
+# Adds new data in 'users' table in database
+def new_users(request):
+    if request.method == 'POST':
+        Name = request.POST.get('name')
+        Email = request.POST.get('email')
+        Role = request.POST.get('role')
+        new = Xusers(Name=Name, Email=Email, Role=Role)
+        new.save()
+    return render(request, 'new_users.html')
+
+# show data of single user from database
+def info_users(request,id):
+    info = Xusers.objects.get(id=id)
+    return render(request, 'users_info.html', {'i':info})
