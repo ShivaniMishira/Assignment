bb7c585ec44a861b31980c915cd7df37d3e7adfb
shivani mishra <mishrasiddh22@gmail.com>
2023-08-31 23:12:55 +0530

    error hendling final commit

--- Assignment/settings.py
+++ Assignment/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*oohd^i_8u9fo9$c--mi(0wgt5x3(@!m5z#z9l_+c9v3yknn^2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
